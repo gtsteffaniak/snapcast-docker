3a27e240f5b8f24aa1db6edcce80441ac11f5e95
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2022-06-25 15:34:08 -0500

    updated server to be general based on ENV

--- snapserver/cleanup.sh
+++ snapserver/cleanup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-server=gworker
 while true; do
     details=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"id": 1,"jsonrpc":"2.0","method":"Server.GetStatus"}' "http://"$server":1780/jsonrpc")
     clientlist=$(echo $details | jq '.result.server.groups[] .clients[]')
@@ -10,7 +9,7 @@ while true; do
         if [ "$disconnected_ids" == "$disconnected_ids_recheck" ]; then
             for id in $disconnected_ids; do
                 echo disconnecting $id
-                curl -s -X POST -H 'Content-Type: application/json' -d '{"id":2,"jsonrpc":"2.0","method":"Server.DeleteClient","params":{"id":'$id'}}' "http://gworker:1780/jsonrpc" >/dev/null 2>&1
+                curl -s -X POST -H 'Content-Type: application/json' -d '{"id":2,"jsonrpc":"2.0","method":"Server.DeleteClient","params":{"id":'$id'}}' "http://$server:1780/jsonrpc" >/dev/null 2>&1
             done
         fi
     fi
