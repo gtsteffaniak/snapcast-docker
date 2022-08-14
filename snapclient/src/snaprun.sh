8d208f09e966878cefbc6be0df315eb243c9ae76
Graham Steffaniak <gtsteff@gmail.com>
2022-08-14 16:22:36 +0100

    Updated docker-compose to not restart clients

--- snapclient/src/snaprun.sh
+++ snapclient/src/snaprun.sh
@@ -135,6 +135,7 @@ function withBluetooth() {
 	# pactl send-message /card/$CARD/bluez switch-codec '"sbc_xq_552"'
 	# force sbc codec... issues occur with other codecs.
 	pactl send-message /card/$CARD/bluez switch-codec '"sbc"'
+	pactl set-port-latency-offset $CARD headset-output 100000 # for stability
 	aplay long_bel.wav
 	VOLUME="125%" # default volume boost
 	################################################
