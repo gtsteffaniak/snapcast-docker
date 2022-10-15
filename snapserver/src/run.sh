219dc5c5434b1ac239005155ebc0a6894d12eb56
Graham Steffaniak <graham.steffaniak@autodesk.com>
2022-10-15 11:17:37 -0500

    updated alpine image

--- snapserver/src/run.sh
+++ snapserver/src/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [ ! -z $buffer ]; then
+if [ -n "$buffer" ]; then
 	if [[ "$buffer" -gt 0 ]]; then
 		sed -i 's/^buffer =.*/buffer = '$buffer'/g' snapserver.conf
 	else
 		echo "invalid buffer setting: $buffer"
 	fi
 fi
-if [ ! -z $chunk_ms ]; then
+if [ -n "$chunk_ms" ]; then
 	if [[ "$chunk_ms" -gt 0 ]]; then
 		sed -i 's/^chunk_ms =.*/chunk_ms = '$chunk_ms'/g' snapserver.conf
 	else
 		echo "invalid chunk setting: $chunk_ms"
 	fi
 fi
-if [ ! -z $codec ]; then
+if [ -n "$codec" ]; then
 	pattern="^flac$|^ogg$|^opus$|^pcm$"
 	if [[ $codec =~ $pattern ]]; then
 		sed -i 's/^codec =.*/codec = '$codec'/g' snapserver.conf
